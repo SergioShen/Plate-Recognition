7ca4c00d5c959aac0ae567b75bcbbe53c414ac86
SergioShen <sjshen@yeah.net>
2017-06-07 16:58:22 +0800

    Create recognition.h

---
+++ recognition.h
@@ -0,0 +1,37 @@
+#ifndef _RECOGNITION_H_
+#define _RECOGNITION_H_
+
+////////////////////////////////////////////////////////////////////////////////
+// includes
+////////////////////////////////////////////////////////////////////////////////
+#include"plate.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// functions
+////////////////////////////////////////////////////////////////////////////////
+
+// find the max index of scr result
+int scr_recognition(const Mat &pred_image);
+
+// find the max index of cnr result
+int cnr_recognition(const Mat &pred_image);
+
+// convert to standard, one-channel, binary image
+Mat plate_image_pre_process(const Mat &src);
+
+// convert to standard, float-valued, i-d array
+Mat scr_image_pre_process(const Mat &src);
+
+// convert to standard, float-valued, i-d array
+Mat cnr_image_pre_process(const Mat &src);
+
+// cut by analysing the values of every row and cloumn
+plate_t plate_rlt_cut_recognition(const Mat &src);
+
+// depth-first-search function
+void dfs(const Mat &src, int x, int y, bool **visited, block &curr_block);
+
+// cut edge surrounds the plate
+Mat cut_edge(const Mat &src);
+
+#endif
